6ea03a13d951f31c16dc4c9251045abe626cc162
Mert Sırakaya <62549656+fswair@users.noreply.github.com>
2021-12-12 17:29:34 +0300

    Update set_func.py

--- set_func.py
+++ set_func.py
@@ -7,79 +7,78 @@ import time
 # birleşim
 # union()
 un = set(input(" : "))
+un1 = set(input(" : "))
 
-def funcSet(u):
-    print(u.union(u))
+def funcSet(x, y):
+    return x.union(y)
 
-funcSet(un)
+print(funcSet(un, un1))
 
 
 # kesişim
 # intersection
 
-un1 = set(input(" : "))
-
-a = un.intersection(un1)
-
-def fonk(ab):
-    print(ab)
-fonk(a)
+def fonk(a, b):
+    ab = un.intersection(un1)
+    return ab
+print(fonk(un, un1))
 
 # difference function
 
-b = un.difference(un1)
 
-def fonk1(ba):
-    print(ba)
-fonk1(b)
+
+def fonk1(x, y):
+    b = x.difference(y)
+    return b
+print(fonk1(un, un1))
 
 
 # symmetric_difference function
-ax = un.symmetric_difference(un1)
-def symm(axc):
-    print(axc)
-symm(ax)
-time.sleep(56)
+
+def symm(x, y):
+    ax = x.symmetric_difference(y)
+    return ax
+print(symm(un, un1))
 
 # alt kümesi mi? issubset function
 
-abx = un1.issubset(un)
 
-def subset(abc):
-    print(abc)
-subset(abx)
+
+def subset(x, y):
+    abx = x.issubset(y)
+    return abx
+print(subset(un1, un))
 
 # classic math functions
 
 # a) kare alma
-x = 10
-v = pow(x,2)
 
-print(v)
+v = pow(10,2) # veya 10 ** 2
+print(v) # 10 ** 2 = 100
 
 # b) üs alma
 
-
-def üs(x, y):
-    g = pow(x,y)
-    print(g,"\n")
+def us(x, y):
+    g = pow(x,y) # veya x ** y
+    return g
 hc = int(input("Taban sayısı giriniz..: "))
 hz = int(input("Üs sayısı giriniz..: "))
-üs(hc,hz)
+print(us(hc,hz))
 
 # karekök alma
 
 def karekok(x):
     f = math.sqrt(x)
-    print(f)
+    return f
 h = int(input("Karekökü alınacak sayı..: "))
-karekok(h)
+print(karekok(h))
+
 # mutlak değer alma
 def mutlak(x):
     d = math.fabs(x)
-    print(d)
+    return d
 hx = int(input("Mutlak değeri alınacak sayı..: "))
-mutlak(hx)
+print(mutlak(hx))
 
 
 # Python | Fonksiyon nasıl yazılır?
@@ -87,12 +86,10 @@ mutlak(hx)
 def function(x,y):
   if(x > y):
     y = x + 1
-    print(y)
-function(15,9)
+    return y
+print(function(15,9))
 # Çıktı: 15, 9'dan büyük olduğu için;
 # y = 9 iken x(15) + 1 olmuştur,
 # Yeni değer 16 olur ve 16 yazdırılır.
 
-
-
-# @FLUSWAIR | GITHUB.COM/FSWAIR
+# GITHUB.COM/FSWAIR
